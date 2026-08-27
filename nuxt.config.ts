a9ea68177c02d4631491242b1d4208d33bc16e10
Mohamed <ghalleb.mohamed2@gmail.com>
2026-08-27 16:34:06 +0200

    refactor: use Nuxt runtime config conventions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,12 +10,12 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseSenderId: process.env.FIREBASE_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseApiKey: '',
+      firebaseAuthDomain: '',
+      firebaseProjectId: '',
+      firebaseStorageBucket: '',
+      firebaseSenderId: '',
+      firebaseAppId: '',
     },
   },
-});
+})
